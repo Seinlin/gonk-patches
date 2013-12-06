f44c83f6ce5a9a7206942dee8e382999adcb03d4
Kai-Zhen Li <kli@mozilla.com>
2013-12-06 14:40:24 +0800

    Rebase nsAppShell

--- patch.sh
+++ patch.sh
@@ -4,4 +4,4 @@ export B2G_TREEID_SH="$PWD/patches/treeid.sh"
 export B2G_HASHED_FILES="$PWD/patches/vendorsetup.sh ${B2G_TREEID_SH} $PWD/.config $PWD/patches/patch.sh"
 export B2G_PATCH_DIRS_OVERRIDE=patches
 
-. patches/vendorsetup.sh
+. patches/vendorsetup.sh $1
